6c15a6f7a0cf4f177463452aa65d7e30d4b3ab52
Ilia <lyssov@mail.ru>
2017-06-16 15:57:52 +0300

    Update BarCode.java

--- BarCode.java
+++ BarCode.java
@@ -1,19 +1,4 @@
-package net.iptp.server.scripts;
-
-
-import net.iptp.server.DAO.Tools;
-import net.iptp.server.Model.Core2.MD;
-import net.iptp.server.Services.Operations.BarReader;
-import org.apache.log4j.Logger;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-
+import BarReader;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,18 +8,11 @@ import java.util.Map;
  *Test of Bar Reader
  */
 public class BarCode {
-    public static Logger log= Logger.getLogger("RequestDispatcher");
 
         public static void main(String[] args) throws SQLException {
-            Tools tools=new Tools(null);
-            tools.Connect();
-        MD md=new MD(tools);
-        tools.setMetadata(md);
 
         BarReader bar = new BarReader();
-        bar.DoBarRead("/home/ilysov/Downloads/bar.pdf","/home/ilysov/Downloads/bar.pdf", tools);
-
-        tools.Disconnect();
+        bar.DoBarRead("bar.pdf", tools);
 
         String [] barcodes = bar.getBarCodes();
 
@@ -43,6 +21,5 @@ public class BarCode {
             }
 
         }
-
 
 }
